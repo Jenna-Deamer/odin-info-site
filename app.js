60b66f207acae4c9804ee52e727794e9b236101c
Jenna-Deamer <jennadeamer@gmail.com>
2025-11-24 16:38:04 -0500

    basic routing

---
+++ app.js
@@ -0,0 +1,36 @@
+const http = require('node:http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+	res.setHeader('Content-Type', 'text/html');
+	let path = './';
+
+    // routing
+    switch(req.url){
+        case '/': path+= 'index.html'
+        break
+        case '/about': path+= 'about.html'
+        break
+        case '/contact': path+= 'contact.html'
+        break
+        default:
+            path+='404.html'
+    }
+
+    // render file
+    fs.readFile(path, (err,data) =>{
+        if(err){
+            console.log(err)
+        }else{
+            res.write(data) 
+            res.end()
+        }
+    })
+});
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
